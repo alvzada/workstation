93ba4c7d1a3d8d51ebbf9bdb3f771e86b51d497c
Nikolai Lvovich <skakoty1337@protonmail.com>
2018-07-28 11:41:20 +0530

    random

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
-if [ ! -x "$(command -v flatpak)" >/dev/null 2>&1 ];
+if [ -x "$(command -v flatpak)" >/dev/null 2>&1 ];
 then
     echo "Setting up flatpak";
+    if [ -r /etc/os-release ];
+    then
+        . /etc/os-release
+        if [ $ID = debian ] || [ $ID = ubuntu ];
+        then
+            sudo apt install flatpak;
+        else
+            echo "Not on Debian based distros";
+            echo "Use your package manager to install 'Flatpak'"
+        fi
+    fi
+
     RET=1;
     until [ ${RET} -eq 0 ]; do
-        sudo apt install gnome-software-plugin-flatpak flatpak;
         flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
-        flatpak --user install flathub org.gnome.Sdk//3.28;
-        flatpak --user install flathub org.gnome.Platform//3.28;
+        echo "";
+        read -p "Enter Sdk/runtime version [Ex: 3.28]: " ver;
+        flatpak --user install flathub org.gnome.Sdk//$ver;
+        flatpak --user install flathub org.gnome.Platform//$ver;
         RET=$?;
     done;
 else
     echo "Flatpak already installed";
 fi
+

c13b2968aef6e4539f4b0f28d41e14d395f41755
Nikolai Lvovich <skakoty1337@protonmail.com>
2018-08-03 21:29:02 +0530

    Updated push.sh

--- push.sh
+++ push.sh
@@ -3,7 +3,8 @@
 if [ -x "$(command -v git)" > /dev/null ];
 then
   read -p "Enter commit message: " message;
-  git add .; git commit -m "$message"; git push -uv origin master
+  git add .; git commit -m "$message"; git push -uv origin master; \
+    git log --full-diff;
 else
   echo "'git' not installed"
 fi

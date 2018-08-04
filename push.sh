15efc307c5ac9dd9260d76a5fc054077a82ccb20
Nikolai Lvovich <nikolaibitinit@gmail.com>
2018-08-04 21:46:59 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -6,11 +6,11 @@ then
     git checkout;
     read -p "Enter commit message: " message;
     git commit -m "$message"; git push -uv origin master; 
-    read -p "Wanna see creepy logs? [y/N]: " choice
+    read -p "\e[36mWanna see creepy logs? [y/N]:\e[0m " choice
     case $choice in
       y|Y) git log --full-diff;;
       n|N) exit;;
-      *) echo "You may drunk now!";;
+      *) echo "\e[35mYou may drunk now!\e[0m";;
     esac
 else
     echo "'git' not installed"

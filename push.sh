2d60bc0ce85a57b32193d922f2f7e2e8d1c4b009
Nikolai Lvovich <nikolaibitinit@gmail.com>
2018-08-04 14:56:02 +0530

    Indented push.sh

--- push.sh
+++ push.sh
@@ -2,11 +2,11 @@
 
 if [ -x "$(command -v git)" > /dev/null ];
 then
-  git add .;
-  git checkout; 
-  read -p "Enter commit message: " message;
-  git commit -m "$message"; git push -uv origin master; \
-    git log --full-diff;
+    git add .;
+    git checkout;
+    read -p "Enter commit message: " message;
+    git commit -m "$message"; git push -uv origin master; \
+        git log --full-diff;
 else
-  echo "'git' not installed"
+    echo "'git' not installed"
 fi

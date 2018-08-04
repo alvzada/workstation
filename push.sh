e505a7c4bf31972761fdc01ab1501d50094d97a3
Nikolai Lvovich <nikolaibitinit@gmail.com>
2018-08-04 21:51:26 +0530

    Updated push.sh

--- push.sh
+++ push.sh
@@ -6,11 +6,10 @@ then
     git checkout;
     read -p "Enter commit message: " message;
     git commit -m "$message"; git push -uv origin master;
-    echo "\e[36mWanna see creepy logs? [y/N]\e[0m: "; 
-    read choice;
+    echo "\e[36mWanna see creepy logs? [y/N]\e[0m: "; read choice;
     case $choice in
       y|Y) git log --full-diff;;
-      n|N) exit;;
+      n|N) echo "Okey, good!"; exit;;
       *) echo "\e[35mYou may drunk now!\e[0m";;
     esac
 else

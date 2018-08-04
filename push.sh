79a6b547d5e6dc8cc77620ea68bce79de1c7938f
Nikolai Lvovich <nikolaibitinit@gmail.com>
2018-08-04 21:42:21 +0530

    push.sh

--- push.sh
+++ push.sh
@@ -5,8 +5,13 @@ then
     git add .;
     git checkout;
     read -p "Enter commit message: " message;
-    git commit -m "$message"; git push -uv origin master; \
-        git log --full-diff;
+    git commit -m "$message"; git push -uv origin master; 
+    read -p "Wanna see creepy logs? [y/N]: " choice
+    case $choice in
+      y|Y) git log --full-diff;;
+      n|N) exit;;
+      *) echo "You may drunk now!";;
+    esac
 else
     echo "'git' not installed"
 fi
